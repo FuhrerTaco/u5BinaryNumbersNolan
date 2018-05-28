69e69d5cb2fa2b82ec3bb391004b6ccae9dafaca
Führer Taco <geniusnolan@hotmail.com>
2018-05-28 06:08:31 -0400

    Finished Program

--- u5BinaryNumbersNolan/Program.cs
+++ u5BinaryNumbersNolan/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/****
+ * Writen by Nolan Meehan
+ * Displays 1-20 as well as the binary equivalent
+ * 
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,11 @@ namespace u5BinaryNumbersNolan
     {
         static void Main(string[] args)
         {
+            for(int i = 1; i <21;i++)
+            {
+                Console.WriteLine(String.Format("{0}    {1}",i, Convert.ToString(i,2)));
+            }
+            Console.ReadLine();
         }
     }
 }
